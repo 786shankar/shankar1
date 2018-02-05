44f09f59b8ea8419f5c5336eafba74ea2ed151b0
786shankar <35800394+786shankar@users.noreply.github.com>
2018-02-05 21:58:51 +0530

    Create Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d=0;
+    printf("enter the number\n");
+    scanf("%d",&a);
+    b=a;
+    while(b!=0)
+    {
+        c=b%10;
+        d=d+c*c*c;
+        b=b/10;
+    }
+    if(a==d)
+    printf(" yes the given number %d is armstrong number",a);
+    else
+    printf(" no the given number %d is not armstrong numner",a);
+}
+
+
+
